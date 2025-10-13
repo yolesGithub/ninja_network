3fc505aed78b7ea84388a1407dbaa86cb118ca4c
yolesGithub <haidardaffa27@gmail.com>
2025-10-13 07:22:08 +0700

    lesson 20

--- resources/views/ninjas/create.blade.php
+++ resources/views/ninjas/create.blade.php
@@ -10,8 +10,9 @@
     <input 
       type="text" 
       id="name" 
-      name="name" 
-      required
+      name="name"
+      value="{{ old('name') }}"
+      required  
     >
 
     <!-- ninja Strength -->
@@ -20,6 +21,7 @@
       type="number" 
       id="skill" 
       name="skill" 
+      value="{{ old('skill') }}"
       required
     >
 
@@ -30,7 +32,7 @@
       id="bio" 
       name="bio" 
       required
-    ></textarea>
+    >{{ old('bio') }}</textarea>
 
     <!-- select a dojo -->
     <label for="dojo_id">Dojo:</label>
@@ -46,6 +48,13 @@
     <button type="submit" class="btn mt-4">Create Ninja</button>
 
     <!-- validation errors -->
-    
+      @if ($errors->any())
+        <ul class="px-4 py-2 bg-red-100">
+          @foreach ($errors->all() as $error)
+            <li class="my-2 text-red-500">{{ $error }}</li>
+          @endforeach
+        </ul>
+      @endif
+      
   </form>
 </x-layout>
